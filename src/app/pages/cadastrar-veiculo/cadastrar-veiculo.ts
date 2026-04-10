e0c4200a9aac339e779ebfd26f4e0b7cf9bf6690
Thomas-Teo <thomasendersom@gmail.com>
2026-04-10 13:12:01 -0300

    feat(form): criado tela de cadastro de veiculo adicionado navegacao pela navbar service renomeado

--- src/app/pages/cadastrar-veiculo/cadastrar-veiculo.ts
+++ src/app/pages/cadastrar-veiculo/cadastrar-veiculo.ts
@@ -1,9 +1,75 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ActivatedRoute, RouterModule } from '@angular/router';
+import { VeiculoService } from '../../service/veiculo-service';
+import { Veiculo } from '../../models/veiculo';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-cadastrar-veiculo',
-  imports: [],
+  standalone: true,
+  imports: [ReactiveFormsModule, RouterModule, CommonModule],
   templateUrl: './cadastrar-veiculo.html',
   styleUrl: './cadastrar-veiculo.scss',
 })
-export class CadastrarVeiculo {}
+export class CadastrarVeiculo implements OnInit{
+  
+  id?: number;
+  form!: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    private service: VeiculoService,
+    private activatedRoute: ActivatedRoute,
+  ) {}
+
+  ngOnInit() {
+    this.form = this.fb.group({
+      modelo: ['', Validators.required],
+      marca: ['', Validators.required],
+      ano: ['', [Validators.required, Validators.min(1900)]],
+      placa: ['',
+        [
+          Validators.required,
+          Validators.pattern(/^([A-Za-z]{3}\d{4}|[A-Za-z]{3}\d[A-Za-z]\d{2})$/)
+        ]
+      ],
+      valor: ['', [Validators.required, Validators.min(0)]]
+    });
+    
+    this.form.get('placa')?.valueChanges.subscribe(value => {
+      if (value) {
+        this.form.get('placa')?.setValue(value.trim().toUpperCase(), { emitEvent: false });
+      }
+    });
+    
+    const idParam = this.activatedRoute.snapshot.paramMap.get('id');
+    this.id = idParam ? Number(idParam) : undefined;
+    if (this.id) {
+      this.service.buscarPorId(this.id).subscribe(res => {
+        this.form.patchValue(res);
+      });
+    }
+  }
+
+    salvar() {
+      if (this.form.invalid) {
+        this.form.markAllAsTouched();
+        return;
+      }
+
+      if (this.form.invalid) return;
+
+      const dados: Veiculo = this.form.value;
+
+      if (this.id) {
+        this.service.atualizar(this.id, dados).subscribe(() => {
+          alert('Atualizado com sucesso');
+        });
+      } else {
+        this.service.criar(dados).subscribe(() => {
+          alert('Criado com sucesso');
+        });
+      }
+    }
+}
---
+++ src/app/service/veiculo-service.ts
@@ -0,0 +1,50 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Veiculo } from '../models/veiculo';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class VeiculoService {
+  
+  private readonly API = 'http://localhost:8080/veiculos';
+
+  constructor(private http: HttpClient) {}
+
+  criar(veiculo: Veiculo): Observable<Veiculo> {
+    return this.http.post<Veiculo>(this.API, veiculo);
+  }
+
+  listar(filtros?: any): Observable<Veiculo[]> {
+    let params = new HttpParams();
+
+    if (filtros) {
+      Object.keys(filtros).forEach(key => {
+        if (filtros[key]) {
+          params = params.set(key, filtros[key]);
+        }
+      });
+    }
+
+    return this.http.get<Veiculo[]>(this.API, { params });
+  }
+
+  buscarPorId(id: number): Observable<Veiculo> {
+    return this.http.get<Veiculo>(`${this.API}/${id}`);
+  }
+
+  buscarComFiltros(filtros: any) {
+    return this.http.get<any[]>(this.API, {
+      params: filtros
+    });
+  }
+
+  atualizar(id: number, veiculo: Veiculo): Observable<Veiculo> {
+    return this.http.put<Veiculo>(`${this.API}/${id}`, veiculo);
+  }
+
+  deletar(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.API}/${id}`);
+  }
+}
